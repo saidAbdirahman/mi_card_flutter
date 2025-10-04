a0f4d85bd8153216ac7cc1f3c4241a701653b0fb
saidAbirahman <saidqaaje11@gmail.com>
2025-10-04 15:28:00 +0300

    Container and some of its properties
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Text('hi'),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.all(50.0),
+            padding: EdgeInsets.only(top: 10.0),
+            child: Text('hello said'),
+          ),
+        ),
       ),
     );
   }
