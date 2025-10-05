107e53aaa2857e54a79c838a9fe5f516521050f9
saidAbirahman <saidqaaje11@gmail.com>
2025-10-05 15:06:26 +0300

    Add cirleAvatar and image
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -15,38 +15,13 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Row(
-            //it is used to shrink the column vertical | size to fit three children size
-            //mainAxisSize: MainAxisSize.min,
-
-            //used to push children up and down and the default is down
-            //verticalDirection: VerticalDirection.up,
-
-            //it controls the main axisis of the layout default start also it has center spaceBetween and others..
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-            //it helps you to align your children to the right
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 30.0,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Container(
-                color: Colors.green,
-                width: 30.0,
-                child: Text('Container 3'),
-              ),
-              Container(
-                color: Colors.grey,
-                width: 30.0,
-                child: Text('Container 3'),
-              ),
-
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.purple,
+                foregroundImage: AssetImage('images/profile.png'),
+              )
             ],
           ),
         ),
@@ -54,3 +29,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//it is used to shrink the column vertical | size to fit three children size
+//mainAxisSize: MainAxisSize.min,
+
+//used to push children up and down and the default is down
+//verticalDirection: VerticalDirection.up,
+
+//it controls the main axisis of the layout default start also it has center spaceBetween and others..
+//mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+//it helps you to align your children to the right
